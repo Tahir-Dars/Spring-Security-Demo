3581dafe77e9ba4ad4cc233d264e3ba80039738f
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-03-01 02:33:51 +0500

    add sayhello method to GreetingController with /hello endpoint

--- src/main/java/controller/GreetingController.java
+++ src/main/java/controller/GreetingController.java
@@ -1,7 +1,13 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
+    @GetMapping("/hello")
+    public String sayhello() {
+        return "Hello Brother";
+    }
 }
