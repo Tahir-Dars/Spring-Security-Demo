4b345b5f27b806e0bcc92e47c9b0f3e29919555c
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-03-01 17:56:07 +0500

    added basic authentication and removed form based for better API Testing

---
+++ src/main/java/bank/springdemo/SecurityConfigurations/SecurityConfigs.java
@@ -0,0 +1,20 @@
+package bank.springdemo.SecurityConfigurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigs {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
