26c4087ace2c5aaad0edc3c460b89c010a7d3f7e
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-03-01 17:14:28 +0500

    remove unnecessary ComponentScan annotation from SpringDemoApplication

--- src/main/java/bank/springdemo/SpringDemoApplication.java
+++ src/main/java/bank/springdemo/SpringDemoApplication.java
@@ -2,13 +2,11 @@ package bank.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"bank.springdemo", "controller"})
 public class SpringDemoApplication {
 
     public static void main(String[] args) {
