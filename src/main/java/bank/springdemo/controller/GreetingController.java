6b50e5404b590cc222fe5f77ef974b5fed27e05c
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-03-06 16:41:11 +0500

    added role based authentication for user and admin

--- src/main/java/bank/springdemo/controller/GreetingController.java
+++ src/main/java/bank/springdemo/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package bank.springdemo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,17 @@ public class GreetingController {
     @GetMapping("/hello")
     public String sayhello() {
         return "Hello Brother";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user/hello")
+    public String sayhelloToUser() {
+        return "Hello user";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin/hello")
+    public String sayhelloToAdmin() {
+        return "Hello Admin";
     }
 }
