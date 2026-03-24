698da0984f7ea1feeedd110c57898ff6a3bff1fc
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-03-24 18:08:09 +0500

    add method to extract JWT from Authorization header

--- src/main/java/bank/springdemo/jwt/jwtUtils.java
+++ src/main/java/bank/springdemo/jwt/jwtUtils.java
@@ -1,4 +1,21 @@
 package bank.springdemo.jwt;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
 public class jwtUtils {
+    private static final Logger logger = LoggerFactory.getLogger(jwtUtils.class);
+
+    //Getting jwt from tokens
+    public String getJwt_FromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        logger.debug("Authorization Header:{}", bearerToken);
+        if (bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 }
