ff76bddaac5fdf6e4dfdef1c4069f2026346a789
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-03-25 13:28:34 +0500

    added code for Key generation for validation

--- src/main/java/bank/springdemo/jwt/jwtUtils.java
+++ src/main/java/bank/springdemo/jwt/jwtUtils.java
@@ -1,12 +1,15 @@
 package bank.springdemo.jwt;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
@@ -14,6 +17,7 @@ import java.util.Date;
 public class jwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(jwtUtils.class);
     private int jwtExpirations_ms;
+    private String jwtSecret;
 
     //Getting tokens from the headers
     public String getJwt_FromHeader(HttpServletRequest request) {
@@ -31,15 +35,22 @@ public class jwtUtils {
         return Jwts.builder().subject(userName)
                 .issuedAt(new Date())
                 .expiration(new Date(new Date().getTime() + jwtExpirations_ms))
-                .signWith(Key()) //{New method in the class will be created for this}
+                .signWith(key()) //{New method in the class will be created for this}
                 .compact();
-        return jwtToken;
     }
 
     //Getting the Username from JWT Token
     public String getUserNameFromJwtToken(String jwtToken) {
-        return Jwts.parser().verifyWith(Key())
+        return Jwts.parser().verifyWith((SecretKey) key())
                 .build().parseSignedClaims(jwtToken)
                 .getPayload().getSubject();
+    }
+
+    //Generating Signing key ^
+    public Key key() {
+        return Keys.hmacShaKeyFor(
+                Decoders.BASE64.decode(jwtSecret)
+        );
+
     }
 }
