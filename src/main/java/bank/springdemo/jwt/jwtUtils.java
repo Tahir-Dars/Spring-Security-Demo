77cbf8bc40a6fac328269e9d74287a3a50027353
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-03-25 12:45:54 +0500

    implemeted the code for token generations using username

--- src/main/java/bank/springdemo/jwt/jwtUtils.java
+++ src/main/java/bank/springdemo/jwt/jwtUtils.java
@@ -1,21 +1,38 @@
 package bank.springdemo.jwt;
 
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
 
 @Component
 public class jwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(jwtUtils.class);
+    private int jwtExpirations_ms;
 
-    //Getting jwt from tokens
+    //Getting tokens from the headers
     public String getJwt_FromHeader(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         logger.debug("Authorization Header:{}", bearerToken);
         if (bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
         }
+        return null;
+    }
+
+    //Generating the tokens using the username
+    public String genTokensFromUserName(UserDetails userDetails) {
+        String userName = userDetails.getUsername();
+        String jwtToken = Jwts.builder().subject(userName)
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime() + jwtExpirations_ms))
+                .signWith(Key()) //{New method in the class will be created for this}
+                .compact();
         return null;
     }
 }
