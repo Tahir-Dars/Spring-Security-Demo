b6f9533a596c146d8d1e97c2dea785c8c73e505e
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-03-26 14:13:48 +0500

    Half impl of AuthTokenFilter

--- src/main/java/bank/springdemo/jwt/AuthTokenFilter.java
+++ src/main/java/bank/springdemo/jwt/AuthTokenFilter.java
@@ -4,14 +4,52 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class AuthTokenFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    private jwtUtils jwtUtilss;
+    private UserDetailsService userDetailsService;
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
 
+    public AuthTokenFilter(jwtUtils jwtUtilss, UserDetailsService userDetailsService) {
+        this.jwtUtilss = jwtUtilss;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        logger.debug("AuthTokenFilter URI:{}", request.getRequestURI());
+        try {
+            String jwt = parsejwt(request);
+            if (!jwt.isEmpty() && jwtUtilss.validateJwtToken(jwt)) {
+                String userName = jwtUtilss.getUserNameFromJwtToken(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource());
+            }
+        } catch (Exception exception) {
+
+        }
+    }
+
+    private String parsejwt(HttpServletRequest request) {
+        String jwt = jwtUtilss.getJwt_FromHeader(request);
+        logger.debug("AuthTokenFilter.java : {}", jwt);
+        return jwt;
     }
 }
