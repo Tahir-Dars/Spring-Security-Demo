2061f2011a5102dcfc98e9df538a142ec7aab0ed
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-03-26 15:41:30 +0500

    Refactor AuthTokenFilter to include authentication event publisher and improve logging

--- src/main/java/bank/springdemo/jwt/AuthTokenFilter.java
+++ src/main/java/bank/springdemo/jwt/AuthTokenFilter.java
@@ -6,10 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -19,11 +20,11 @@ import java.io.IOException;
 @Component
 public class AuthTokenFilter extends OncePerRequestFilter {
 
-    private jwtUtils jwtUtilss;
-    private UserDetailsService userDetailsService;
+    private final jwtUtils jwtUtilss;
+    private final UserDetailsService userDetailsService;
     private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
 
-    public AuthTokenFilter(jwtUtils jwtUtilss, UserDetailsService userDetailsService) {
+    public AuthTokenFilter(jwtUtils jwtUtilss, UserDetailsService userDetailsService, DefaultAuthenticationEventPublisher authenticationEventPublisher) {
         this.jwtUtilss = jwtUtilss;
         this.userDetailsService = userDetailsService;
     }
@@ -40,11 +41,14 @@ public class AuthTokenFilter extends OncePerRequestFilter {
                 UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
                         new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                logger.debug("Roles from JWT:{}", userDetails);
             }
         } catch (Exception exception) {
-
+            logger.debug("Can't set user authentication: {}", exception.getMessage());
         }
+        filterChain.doFilter(request, response);
     }
 
     private String parsejwt(HttpServletRequest request) {
