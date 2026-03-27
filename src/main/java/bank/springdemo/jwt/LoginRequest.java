d2fe46e2e38e8c01af3ca0fe4c7342a48889549e
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-03-27 16:52:43 +0500

    added the code for loginRequest and LoginResponse

--- src/main/java/bank/springdemo/jwt/LoginRequest.java
+++ src/main/java/bank/springdemo/jwt/LoginRequest.java
@@ -1,4 +1,13 @@
 package bank.springdemo.jwt;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
+    private String userName;
+    private String password;
 }
--- src/main/java/bank/springdemo/jwt/LoginResponse.java
+++ src/main/java/bank/springdemo/jwt/LoginResponse.java
@@ -1,4 +1,17 @@
 package bank.springdemo.jwt;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginResponse {
+    private String jwtToken;
+    private String userName;
+    private List<String> roles;
+
 }
